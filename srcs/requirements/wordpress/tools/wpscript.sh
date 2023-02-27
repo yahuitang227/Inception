15cf688ec9b93eaa44027438ab57fd01a9c3c83b
Ya-hui Tang <yahuitang227@gmail.com>
2023-02-28 00:52:46 +0100

    donegit add *

--- srcs/requirements/wordpress/tools/wpscript.sh
+++ srcs/requirements/wordpress/tools/wpscript.sh
@@ -8,11 +8,13 @@ cd /var/www/html/wordpress
 wp config create	--allow-root --dbname=${SQL_DATABASE} \
 			--dbuser=${SQL_USER} \
 			--dbpass=${SQL_PASSWORD} \
-			--dbhost=${SQL_HOST} 
+			--dbhost=${SQL_HOST} \
+			--url=https://${DOMAIN_NAME}
+			
 
 #if ! wp core is-installed; then
 wp core install	--allow-root \
-			--url=${DOMAIN_NAME} \
+			--url=https://${DOMAIN_NAME} \
 			--title=${SITE_TITLE} \
 			--admin_user=${ADMIN_USER} \
 			--admin_password=${ADMIN_PASSWORD} \
